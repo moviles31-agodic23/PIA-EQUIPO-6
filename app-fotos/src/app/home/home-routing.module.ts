441c352e2b1fc74a31f1a589444b838eaf1cd56d
Emilio Madrid <emilio.hernandezmd@uanl.edu.mx>
2023-11-15 23:14:56 -0600

    Routing

--- app-fotos/src/app/home/home-routing.module.ts
+++ app-fotos/src/app/home/home-routing.module.ts
@@ -2,10 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
+import { LoginComponent } from '../login/login.component';
+import { FeedComponent } from '../feed/feed.component';
+import { PublicarFotoComponent } from '../publicar-foto/publicar-foto.component';
+import { PerfilDeUsuarioComponent } from '../perfil-de-usuario/perfil-de-usuario.component';
+
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'feed',
+        component: FeedComponent,
+      },
+      {
+        path: 'publicar',
+        component: PublicarFotoComponent,
+      },
+      {
+        path: 'perfil',
+        component: PerfilDeUsuarioComponent,
+      }
+    ],
   }
 ];
 
